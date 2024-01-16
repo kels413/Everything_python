dcf079c4c4a246cc31abb13ac81a37aa6c08f599
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 15:29:06 +0100

    working

--- regex/regex4.py
+++ regex/regex4.py
@@ -1,4 +1,4 @@
-
+#!/usr/local/bin/python3
 # Design a regular expression pattern to validate a password that must contain
 # at least one uppercase letter, one lowercase letter, and one digit,
 # with a minimum length of 8 characters.
