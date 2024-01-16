13c51a160d483a5a490565f93118de1ca45ebe5c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 14:57:15 +0100

    working

--- regex/regex2.py
+++ regex/regex2.py
@@ -16,7 +16,7 @@ import re
 
 def match_string(text):
     pattern = re.compile(r'cat|dog')
-    match = pattern.findall(text, re.IGNORECASE)
+    match = pattern.search(text, re.IGNORECASE)
     if match:
         print(match)
 
