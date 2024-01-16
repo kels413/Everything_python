d3208753df7a935cc7efa0945a11fa25338d6448
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 15:52:00 +0100

    updated regex5

---
+++ regex5.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3
+
+# Develop a regular expression pattern that
+# matches a string containing any number of repetitions of 
+# the word "apple," followed by the word "pie."
+
+
+def match_string(text):
+    pass
