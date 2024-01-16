9e3ef380e559ee3eb4b46253af96c95e76542981
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 15:28:56 +0100

    working

--- regex/regex4.py
+++ regex/regex4.py
@@ -1,9 +1,10 @@
-Design a regular expression pattern to validate a password that must contain
-at least one uppercase letter, one lowercase letter, and one digit,
-with a minimum length of 8 characters.
 
-Example strings:
+# Design a regular expression pattern to validate a password that must contain
+# at least one uppercase letter, one lowercase letter, and one digit,
+# with a minimum length of 8 characters.
 
-"Passw0rd"
-"SecurePa55"
-"weakpassword"
+# Example strings:
+
+# "Passw0rd"
+# "SecurePa55"
+# "weakpassword"
