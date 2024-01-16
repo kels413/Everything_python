66c95bfb57f711f461af2512a3d0451e99e38873
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 15:10:23 +0100

    working

--- regex/regex3.py
+++ regex/regex3.py
@@ -1,7 +1,8 @@
-Provide a regular expression pattern to match a phone number in the format (XXX) XXX-XXXX, where X represents a digit.
+# Provide a regular expression pattern to match
+ a phone number in the format (XXX) XXX-XXXX, where X represents a digit.
 
-Example strings:
+# Example strings:
 
-"(123) 456-7890"
-"Phone: (987) 654-3210"
-"Invalid: 555-1234"
+# "(123) 456-7890"
+# "Phone: (987) 654-3210"
+# "Invalid: 555-1234"
