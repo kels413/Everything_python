5a5bdf2a835879ad4a6fb5362bcb4df2403cea4e
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 14:50:06 +0100

    update

--- regex/regex2.py
+++ regex/regex2.py
@@ -14,7 +14,7 @@ import re
 # "Both Cat and Dog are pets."
 
 def match_string(text):
-    pattern = re.compile(r'cat | Dog')
+    pattern = re.compile(r'cat | dog')
     match = pattern.search(text, IGNORE)
     if match:
         print(match.group())
