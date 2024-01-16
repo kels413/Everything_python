43c035af87e369a146e746d793fd4ccf5efc07a9
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 15:07:37 +0100

    working

--- regex/regex2.py
+++ regex/regex2.py
@@ -18,7 +18,7 @@ def match_string(text):
     pattern = re.compile(r'cat | dog', re.IGNORECASE)
     match = pattern.search(text)
     if match:
-        print(match.group(1))
+        print(match.group())
 
 match_string("A Dog barked loudly cat Dog")
 match_string("The cat is playful. is cat")
