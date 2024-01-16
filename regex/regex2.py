43aa2d209cc7eb6101b739edc46eb8182511b8bb
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 15:04:05 +0100

    working

--- regex/regex2.py
+++ regex/regex2.py
@@ -18,7 +18,7 @@ def match_string(text):
     pattern = re.compile(r'cat|dog', re.IGNORECASE)
     match = pattern.search(text)
     if match:
-        print(match.group)
+        print(match)
 
 match_string("A Dog barked loudly cat")
 
