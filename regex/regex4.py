be6026c33b8b728410a10848756430398562df8e
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 15:28:43 +0100

    working

---
+++ regex/regex4.py
@@ -0,0 +1,7 @@
+Design a regular expression pattern to validate a password that must contain at least one uppercase letter, one lowercase letter, and one digit, with a minimum length of 8 characters.
+
+Example strings:
+
+"Passw0rd"
+"SecurePa55"
+"weakpassword"
