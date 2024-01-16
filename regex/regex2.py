919d843f71da2e579615afc9a089cff9c6db73a5
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 14:53:46 +0100

    working

--- regex/regex2.py
+++ regex/regex2.py
@@ -20,8 +20,8 @@ def match_string(text):
     if match:
         print(match.group())
 
-match_string("The cat is playful.")
 match_string("A Dog barked loudly.")
+match_string("The cat is playful.")
 match_string("Both Cat and Dog are pets.")
 
  
