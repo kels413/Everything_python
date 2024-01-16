a44acc3947507b64652ffb064e53a183853aaeab
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 14:59:41 +0100

    working

--- regex/regex2.py
+++ regex/regex2.py
@@ -17,7 +17,7 @@ import re
 def match_string(text):
     pattern = re.compile(r'cat|dog')
     matches = pattern.findall(text, re.IGNORECASE)
-    for  match in matches:
+    for match in matches:
         print(match)
 
 match_string("A Dog barked loudly.")
