bc577b6d1e414b8538bb16e13f0ecf2c81d31457
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 14:55:03 +0100

    working

--- regex/regex2.py
+++ regex/regex2.py
@@ -21,8 +21,8 @@ def match_string(text):
         print(match.group())
 
 match_string("A Dog barked loudly.")
-# match_string("The cat is playful.")
-# match_string("Both Cat and Dog are pets.")
+match_string("The cat is playful.")
+match_string("Both Cat and Dog are pets.")
 
  
 
