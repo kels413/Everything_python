5424daa7455a08219decf29333269212c5ce2b84
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-16 15:22:27 +0100

    working

--- regex/regex3.py
+++ regex/regex3.py
@@ -18,6 +18,6 @@ def is_phoneNumbers(text):
   
     
 is_phoneNumbers("(123) 456-7890")
-# is_phoneNumber("Phone: (987) 654-3210")
-# is_phoneNumber("Invalid: 555-1234")
+is_phoneNumber("Phone: (987) 654-3210")
+is_phoneNumber("Invalid: 555-1234")
 print("hello world")
