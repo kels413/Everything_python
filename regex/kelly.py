fec9c461d8c5bc47651ba672f17114a30774a6f7
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-17 13:54:30 +0100

    updated regex5

--- regex/kelly.py
+++ regex/kelly.py
@@ -1,8 +1,8 @@
 #!/usr/local/bin/python3
 # name.py
 
-def hello():
-    print("Hello from the module!")
+# def hello():
+#     print("Hello from the module!")
 
-print(f"In main module, __name__ is {__name__}")
+# print(f"In main module, __name__ is {__name__}")
 
