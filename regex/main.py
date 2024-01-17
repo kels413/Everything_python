c24887939f2a208c2bcba9be7bd0f8222c23612b
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-17 13:42:53 +0100

    updated regex5

--- regex/main.py
+++ regex/main.py
@@ -60,8 +60,8 @@ import pydoc
 """this is the module"""
 # main_script.py
 
-import name
+import regex.if as if
 
 print(globals())
-name.hello()
+if.hello()
 print(f"In the main script, __name__ is {__name__}")
