d03e73d8e9308b8644270b8f63586926d35958ff
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-17 13:23:33 +0100

    updated regex5

--- regex/main.py
+++ regex/main.py
@@ -1,60 +1,60 @@
-#!/usr/local/bin/python3
-import re
-"""
-to match this pattern (415-555-4242) without regex
-we have to do something like this
-"""
+# #!/usr/local/bin/python3
+# import re
+# """
+# to match this pattern (415-555-4242) without regex
+# we have to do something like this
+# """
 
-# def is_phoneNumber(number):
-#     if number.isdecimal():
-#         return False
-#     for num in range(0, 3):
-#         if not number[num].isdecimal():
-#             return False
-#         if number[3] != '-':
-#             return False
-#     for num in range(4,7):
-#         if not number[num].isdecimal():
-#             return False
-#         if number[7] != '-':
-#            return False
-#     for num in range(8, 12):
-#         if not number[num].isdecimal():
-#            return False
-#     return True
+# # def is_phoneNumber(number):
+# #     if number.isdecimal():
+# #         return False
+# #     for num in range(0, 3):
+# #         if not number[num].isdecimal():
+# #             return False
+# #         if number[3] != '-':
+# #             return False
+# #     for num in range(4,7):
+# #         if not number[num].isdecimal():
+# #             return False
+# #         if number[7] != '-':
+# #            return False
+# #     for num in range(8, 12):
+# #         if not number[num].isdecimal():
+# #            return False
+# #     return True
        
-# phone_number = is_phoneNumber('415-555-4242')
-# not_phone_number = is_phoneNumber("thi is a valid phone number 415-555-4242 Mistar Kelly")
+# # phone_number = is_phoneNumber('415-555-4242')
+# # not_phone_number = is_phoneNumber("thi is a valid phone number 415-555-4242 Mistar Kelly")
 
 
-# print(phone_number)
-# print(not_phone_number)
+# # print(phone_number)
+# # print(not_phone_number)
 
-#we can simplify this long process just by doing this 
+# #we can simplify this long process just by doing this 
 
-# phone_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# # phone_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 
-def find_number(number):
-    # phone_regex2 = re.compile(r'(\(\d{3}\))-(\d{3}-\d{4})')
-    # phone_regex = re.compile(r'(\(\d\d\d\))-(\d\d\d-\d\d\d\d)')
-    pattern = re.compile(r'((\d{3}\))-?(\d{6}-\d{4})')
-    found = pattern.search(number)
-    if found:
-        # mo , ma = found.groups()
-        print("string found {}".format(found.group()))
-    else:
-        print("string not found")
+# def find_number(number):
+#     # phone_regex2 = re.compile(r'(\(\d{3}\))-(\d{3}-\d{4})')
+#     # phone_regex = re.compile(r'(\(\d\d\d\))-(\d\d\d-\d\d\d\d)')
+#     pattern = re.compile(r'((\d{3}\))-?(\d{6}-\d{4})')
+#     found = pattern.search(number)
+#     if found:
+#         # mo , ma = found.groups()
+#         print("string found {}".format(found.group()))
+#     else:
+#         print("string not found")
 
-find_number("I'm going somewhere tonight by the  (415)-555-4242")
-find_number("I'm going somewhere tonight by the  415555-4242")
+# find_number("I'm going somewhere tonight by the  (415)-555-4242")
+# find_number("I'm going somewhere tonight by the  415555-4242")
 
-# batwoman or batman or batman123?
+# # batwoman or batman or batman123?
 
 
 
-# if phone_regex.search("moshi is 415-555-4243 a boy "):
-#    print("phone number found")
-# else:
-#     print("phone number not found")
+# # if phone_regex.search("moshi is 415-555-4243 a boy "):
+# #    print("phone number found")
+# # else:
+# #     print("phone number not found")
 
 
