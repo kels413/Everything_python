57a69c669d02bd456f041de0f7d5f311cd7a5c21
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-17 13:50:28 +0100

    updated regex5

--- regex/main.py
+++ regex/main.py
@@ -68,5 +68,5 @@ print(f"In the main script, __name__ is {__name__}")
 # answer = globals()
 for key, value in list(globals().items()):
     # if key.startswith('__'):
-        print(key, value)
+    print(key, value)
 
