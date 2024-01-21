01a82e685d1ff86a036d3bc2d685ab1d4d200187
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-21 23:33:30 +0100

    working

--- regex/testing.py
+++ regex/testing.py
@@ -12,7 +12,7 @@ print("this is for testing the globals dictionary")
 
 lists = ["kelly", "okoye", "sinner", "emma"];
 
-for i in range(0, 10):
+for i in lists:
      print(i)
 
 if __name__ == "__main__":
