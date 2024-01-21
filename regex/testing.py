f466ad6edb56dd98d7b4baedde475f7b9f138b0b
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-21 23:33:14 +0100

    working

--- regex/testing.py
+++ regex/testing.py
@@ -10,7 +10,7 @@ for k, v in globals().copy().items():
 
 print("this is for testing the globals dictionary")
 
-lists = ["kelly", "okoye", ""]
+lists = ["kelly", "okoye", "sinner", ""]
 
 for i in range(0, 10):
      print(i)
