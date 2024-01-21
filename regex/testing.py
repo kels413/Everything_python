86fd6df1e1e3cb6925d32ed844e2553fadfef1d5
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-21 23:45:38 +0100

    working

--- regex/testing.py
+++ regex/testing.py
@@ -1,25 +1,26 @@
-#!/usr/local/bin/python3
+# #!/usr/local/bin/python3
 
-def hello():
-    print("hello world")
-
-
-num = 5
-num2 = 0
-
-sum = num / num2
-
-print(sum)
-for k, v in globals().copy().items():
-        print(k, v)
+# def hello():
+#     print("hello world")
 
 
-print("this is for testing the globals dictionary")
-names = ["kelly", "okoye", "sinner", "emma"]
+# num = 5
+# num2 = 0
 
-for index, name in enumerate(names):
-   print(name)
+# sum = num / num2
+
+# print(sum)
+# for k, v in globals().copy().items():
+#         print(k, v)
 
 
-if __name__ == "__main__":
-    hello()
+# print("this is for testing the globals dictionary")
+# names = ["kelly", "okoye", "sinner", "emma"]
+
+# for index, name in enumerate(names):
+#    print(name)
+
+
+# if __name__ == "__main__":
+#     hello()
+
