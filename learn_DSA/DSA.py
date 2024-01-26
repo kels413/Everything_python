33ff33cff5667e346a2abc185c765ba14d2bda2b
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-26 21:39:14 +0100

    working

--- learn_DSA/DSA.py
+++ learn_DSA/DSA.py
@@ -1,5 +1,9 @@
-number = 2
-while number <= 100:
-    if number % 2 == 0:
-        print(number)
-    number += 1
+def print_even():
+    number = 2
+    while number <= 100:
+        if number % 2 == 0:
+            print(number)
+        number += 1
+
+
+print_even()
