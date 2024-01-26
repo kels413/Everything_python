4bde4d134dea18c0a10741006721e12749e3792b
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-26 21:36:26 +0100

    working

--- learn_DSA/DSA.py
+++ learn_DSA/DSA.py
@@ -1,5 +1,7 @@
 number = 2
 
 while number <= 100:
-# If number is even, print it: if number % 2 == 0:
-print(number) number += 1
+    print(number)
+    if number % 2 == 0:
+        print(number)
+        number += 1
