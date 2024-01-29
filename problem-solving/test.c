db804bb3bdee95ba38045b0ceacfbb9936e52890
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 00:48:11 +0100

    problem_solving

--- problem-solving/test.c
+++ problem-solving/test.c
@@ -6,7 +6,17 @@
 int main(void)
 {
 
-    int array = {1,2,3,4,5};
+    int array[] = {1,2,3,4,5};
+    
+    int sum = 0;
+    for (size_t i = 0; i < 5; i++)
+    {
+        /* code */
+
+
+        printf("sum is %d", sum += i);
+    }
+    
 
     return 0;
 }
