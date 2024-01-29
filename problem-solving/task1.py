219daf7f5d043853d9e26a66623d33d9ae2e90ae
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 00:50:12 +0100

    problem_solving

--- problem-solving/task1.py
+++ problem-solving/task1.py
@@ -2,9 +2,11 @@
 Create a function that takes a list of numbers as input and returns the sum of all the elements in the list.
 """
 
-# def list_sum(lists = []):
-#     for i in lists:
-#         print(f"sum is {i+}")
+def list_sum(lists = []):
+    sum = 0
+    for i in lists:
+        sum += i
+    print(f"sum of the list is == {sum}")
 
 
 # list_sum([1,2,3,4,5])
