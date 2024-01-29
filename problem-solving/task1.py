e70cebad212d6500536d9be387e472c6ba6677e9
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 00:38:09 +0100

    problem_solving

--- problem-solving/task1.py
+++ problem-solving/task1.py
@@ -2,3 +2,6 @@
 Create a function that takes a list of numbers as input and returns the sum of all the elements in the list.
 """
 
+def list_sum(lists = []):
+    try:
+        if not isinstance(lists)
