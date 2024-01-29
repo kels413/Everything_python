58a65fe808fa47a0e5857f26731ce11ba1818a37
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 00:47:11 +0100

    problem_solving

--- problem-solving/task1.py
+++ problem-solving/task1.py
@@ -2,9 +2,11 @@
 Create a function that takes a list of numbers as input and returns the sum of all the elements in the list.
 """
 
-def list_sum(lists = []):
-    for i in lists:
-        print(f"sum is {i+}")
+# def list_sum(lists = []):
+#     for i in lists:
+#         print(f"sum is {i+}")
 
 
-list_sum([1,2,3,4,5])
+# list_sum([1,2,3,4,5])
+
+
---
+++ problem-solving/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(void)
+{
+
+    int array = {1,2,3,4,5};
+
+    return 0;
+}
