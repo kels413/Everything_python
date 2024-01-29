cb050c9c29535fce44e053ec00798004d44ab19d
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 00:57:14 +0100

    problem_solving

--- problem-solving/task1.py
+++ problem-solving/task1.py
@@ -2,18 +2,21 @@
 Create a function that takes a list of numbers as input and returns the sum of all the elements in the list.
 """
 
-def list_sum(lists = []):
-    sum = 0
-    if isinstance(lists, list):
-        for i in lists:
-            sum += i
-        return sum
-    else:
-        print("not allowed")
+# def list_sum(lists = []):
+#     sum = 0
+#     if isinstance(lists, list):
+#         for i in lists:
+#             sum += i
+#         return sum
+#     else:
+#         print("not allowed")
     
 
-# total = list_sum("ikjoijn")
-total = list_sum([1,2,3,4,5])
-print(f"The Total is {total}")
+# # total = list_sum("ikjoijn")
+# total = list_sum([1,2,3,4,5])
+# print(f"The Total is {total}")
+
+
+
 
 
