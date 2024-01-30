76e7d4ee358d93979233515752c52e5f290b5003
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 01:32:59 +0100

    tested out the testing on normal list

---
+++ problem-solving/task2.py
@@ -0,0 +1,9 @@
+"""
+List Reversal:
+Write a program that reverses a given list. For example, if the input is [1, 2, 3, 4], the output should be [4, 3, 2, 1].
+"""
+
+names = [1,2,3,4,5]
+
+names.reverse()
+print(names)
