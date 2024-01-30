420ef533e27315123622be189098be7aca732b5c
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-30 01:38:23 +0100

    tested out the testing on normal list

--- problem-solving/task2.py
+++ problem-solving/task2.py
@@ -6,6 +6,8 @@ Write a program that reverses a given list. For example, if the input is [1, 2, 
 names = [1,2,3,4,5]
 
 pop_item = names.pop()
+pop_item2 = names.pop()
 print(pop_item)
+print(pop_item2)
 # names.reverse()
 # print(names)
