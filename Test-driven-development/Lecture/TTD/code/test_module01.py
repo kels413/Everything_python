2f39bc44d8866305e8e8109f6f15e8c69b0a232e
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-13 14:52:14 +0100

    TTD

---
+++ Test-driven-development/Lecture/TTD/code/test_module01.py
@@ -0,0 +1,17 @@
+"""Learning bout doctest in python
+"""
+
+class TestClass01:
+    """This is the TestClass01"""
+    
+    def test_case01(self):
+        """this is the test_case01"""
+    
+    def test_function01(self):
+        """this is the test_function01"""
+
+    
+
+    
+
+    
---
+++ Test-driven-development/Lecture/test.py
@@ -0,0 +1,22 @@
+
+def add_two(num1, num2):
+
+    """
+    >>> add_two(5 , 5)
+    10
+    >>> add_two(4,4)
+    8
+    """
+    return num1 + num2
+def user_name(name):
+    """
+    >>> user_name("kelly")
+    'this is your name kelly'
+    """
+    return f"this is yours name {name}"
+
+# Example usage
+result = user_name("kelly")
+print(result)
+
+    
