e9a974a3b8de006d3135ee41ac88002d645fb413
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-20 22:11:07 +0100

    defined procedres of creating a table using sql alchemy

--- sqlalchemy/orm.py
+++ sqlalchemy/orm.py
@@ -1,11 +1,16 @@
 """
 steps:
     1: import sqlalchemy
-    2. import columns, strings, intergers, primary key
+    2. import columns, strings, intergers, primary key, create_engine
     3. import declartive base
     4. create a connection
     5. create a class and inherit from the declarative class
     6. call the declarative class (Base) and create all Table.
 """
+
+import sqlalchemy
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
 
 
