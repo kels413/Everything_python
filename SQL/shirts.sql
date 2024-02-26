3d577160bc46898153effd058758f996e0ff9f74
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-02-26 17:05:36 +0100

    created a db

---
+++ SQL/shirts.sql
@@ -0,0 +1,19 @@
+-- a sample database for managing my clothes
+-- this db helps me to monitor my shirt, know the type of shirt -- it is, the shirt -- color, the shirt size and  how many times I have worn a cloth
+-- so i can decide whether to give it out do someother stuff with the shirt.
+
+-- create the DATABASE
+CREATE DATABASE IF NOT EXISTS shirts_db;
+
+USE shirts_db;
+
+CREATE TABLE IF NOT EXISTS shirts
+(
+    shirts_id INT NOT NULL AUTO_INCREMENT,
+)
+
+
+
+
+
+
